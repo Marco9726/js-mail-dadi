64fb1ddfa05904196520ebdabfd235cd3d892968
Marco 9726 <marcosarra9@gmail.com>
2022-11-30 17:56:18 +0100

    esercizio mail fatto

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,20 +1,22 @@
 //1 - creare un array con una lista di email 
 const list = ['pippo@gmail.com', 'paperino@icloud.it', 'topolino@yahoo.it'];
 //2 - chiere all'utente di inserire una email vailda
-const email_utente = prompt('Inresisci la tua email');
+const user_email = prompt('Inresisci la tua email');
 //3 - confrontare se la mail inserita fa parte della lista dell'array
-let found = false
+//3.1 - creare un cilo for di ricerca per la lista dell'array
+let found = false;
 for ( i=0; i < list.length; i++){
-    const email_list = list[i]
-    if ( email_utente == email_list) {
+    //3.2 - creare la variabile della singola mail della lista dell'array e confrontarla con la mail inserita dall'utente 
+    const email = list[i]
+    if ( user_email === email) {
         found = true
     }
 }
-
+//3.2.1 - se le email corrispondono stampare "Puoi partecipare"
 if(found) {
     console.log('Puoi partecipare');
 }
-
+//3.2.2 - se non corrispondono stampare "Non puoi partecipare"
 else {
-    console.log('Non puoi parteciparer');
+    console.log('Non puoi partecipare');
 }
--- mail/readme.txt
+++ mail/readme.txt
@@ -2,3 +2,7 @@ Chiedi all’utente la sua email, controlla che sia nella lista di chi può acce
 //1 - creare un array con una lista di email 
 //2 - chiere all'utente di inserire una email vailda
 //3 - confrontare se la mail inserita fa parte della lista dell'array
+    //3.1 - creare un cilo for di ricerca per la lista dell'array
+    //3.2 - creare la variabile della singola mail della lista dell'array e confrontarla con la mail inserita dall'utente   
+        //3.2.1 - se le email corrispondono stampare "Puoi partecipare"
+        //3.2.2 - se non corrispondono stampare "Non puoi partecipare"
